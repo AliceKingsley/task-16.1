c1ce691ce991dfb77a88b42cdfb604ec045e0a13
Марина Максимова <m.maksimova@m-maksimova.local>
2022-07-26 19:07:32 +0300

    Без Добавить

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const input = document.querySelector('input');
-const buttonAdd = document.querySelector('.add');
+// const buttonAdd = document.querySelector('.add');
 const buttonFinish = document.querySelector('.finish');
 const div = document.querySelector('.error');
 const divArray = document.querySelector('.array');
@@ -8,27 +8,32 @@ const divSum = document.querySelector('.sum');
 let values = [];
 let sum = 0;
 
-buttonAdd.addEventListener("click", putValues);
+// buttonAdd.addEventListener("click", putValues);
 
-function putValues() {
-    if ((input.value === '') || (!isFinite(input.value)) || (input.value === null )) {
-        buttonAdd.disabled = true;
-        div.textContent = 'Ввод завершён. Нажмите "Готово"';
-        values.pop();
-    } 
-}
+// function putValues() {
+//     if ((input.value === '') || (!isFinite(input.value)) || (input.value === null )) {
+//         buttonAdd.disabled = true;
+//         div.textContent = 'Ввод завершён. Нажмите "Готово"';
+//         values.pop();
+//     } 
+// }
 
 input.addEventListener("change", getValues);
 
 function getValues() {
     values.push(input.value);
     console.log(values);
+    if ((input.value === '') || (!isFinite(input.value)) || (input.value === null )) {
+        values.pop();
+        div.textContent = 'Ввод завершён. Нажмите "Готово"';
+        input.removeEventListener("change", getValues);
+    }
 }
 
 buttonFinish.addEventListener("click", sumInput);
 
 function sumInput() {
-    buttonAdd.disabled = true;
+    // buttonAdd.disabled = true;
 
     divArray.textContent = `Отсортированный по возрастанию массив: [${values.sort()}]`;
 
