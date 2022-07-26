58292e123fc5e093f11e576d2fc41942cdab0a5d
Марина Максимова <m.maksimova@m-maksimova.local>
2022-07-26 13:39:32 +0300

    Задание 1 версия с Добавить

---
+++ index.js
@@ -0,0 +1,42 @@
+const input = document.querySelector('input');
+const buttonAdd = document.querySelector('.add');
+const buttonFinish = document.querySelector('.finish');
+const div = document.querySelector('.error');
+const divArray = document.querySelector('.array');
+const divSum = document.querySelector('.sum');
+
+let values = [];
+let sum = 0;
+
+buttonAdd.addEventListener("click", putValues);
+
+function putValues() {
+    if ((input.value === '') || (!isFinite(input.value)) || (input.value === null )) {
+        buttonAdd.disabled = true;
+        div.textContent = 'Ввод завершён. Нажмите "Готово"';
+        values.pop();
+    } 
+}
+
+input.addEventListener("change", getValues);
+
+function getValues() {
+    values.push(input.value);
+    console.log(values);
+}
+
+buttonFinish.addEventListener("click", sumInput);
+
+function sumInput() {
+    buttonAdd.disabled = true;
+
+    divArray.textContent = `Отсортированный по возрастанию массив: [${values.sort()}]`;
+
+    for (let i = 0; i < values.length; i++) {
+        sum += Number(values[i]);
+    }
+    divSum.textContent = `Сумма элементов: ${sum}`;
+
+    buttonFinish.disabled = true;
+    div.textContent = 'Для повтора обновите страницу';
+}
